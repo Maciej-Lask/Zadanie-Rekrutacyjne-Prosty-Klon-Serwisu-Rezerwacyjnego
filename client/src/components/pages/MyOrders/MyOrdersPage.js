8dff8409c291ae1d3f310c6189f2f2ad13146d96
Maciej Laskowski <maciek30088@gmail.com>
2024-01-28 21:36:29 +0100

    Fix the delete bug

--- client/src/components/pages/MyOrders/MyOrdersPage.js
+++ client/src/components/pages/MyOrders/MyOrdersPage.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Container, Spinner } from 'react-bootstrap';
+import { Button, Container, Spinner, Modal } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getAllAds } from '../../../redux/adsRedux';
 import { API_URL, IMAGES_URL } from '../../../config';
 import styles from './MyOrdersPage.module.scss';
 import { FaEye, FaRegTrashAlt, FaLocationArrow } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const MyOrders = () => {
   const [reservations, setReservations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [reloadFlag, setReloadFlag] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedReservationId, setSelectedReservationId] = useState(null);
+
+  const navigate = useNavigate();
 
   const ads = useSelector((state) => getAllAds(state));
 
@@ -32,7 +37,7 @@ const MyOrders = () => {
         setError(error.message);
         setLoading(false);
       });
-  }, []);
+  }, [reloadFlag]);
 
   const getAdData = (adId) => {
     const ad = ads.find((ad) => ad._id === adId);
@@ -48,11 +53,42 @@ const MyOrders = () => {
   };
 
   const handleDelete = (reservationId) => {
-   
-  }
-  const handleEdit = (reservationId) => {
-    
-  }
+    setSelectedReservationId(reservationId); 
+    setShowModal(true); 
+  };
+
+  const confirmDelete = () => {
+    console.log('delete');
+    console.log(selectedReservationId);
+    const options = {
+      method: 'DELETE',
+      credentials: 'include',
+    };
+    fetch(`${API_URL}api/reservations/${selectedReservationId}`, options)
+      .then((res) => {
+        console.log('Response:', res);
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        
+        if (res.status !== 204) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          console.log('Data:', data);
+        } else {
+          console.log('Success');
+          setReloadFlag(!reloadFlag);
+        }
+      })
+      .catch((err) => {
+        console.error('Fetch error:', err);
+      });
+    setShowModal(false); 
+  };
+
   return (
     <Container>
       <h1>Your orders</h1>
@@ -68,24 +104,49 @@ const MyOrders = () => {
             <div key={reservation._id} className={styles.orderItem}>
               {getAdData(reservation.adInfo)}
               <div className={styles.orderInfo}>
-              <Button
-                variant="outline-dark"
-                as={Link}
-                to={`/ad/${reservation.adInfo}`}
-              >
-                <FaEye className={styles.svgIcon} />
-              </Button>
-              <p>Date: {reservation.reservationDate}</p>
-              <p>Time: {reservation.reservationTime}</p>
-              <Button variant="outline-dark" onClick={handleEdit(reservation._id)}>Edit reservation</Button>
-              <Button variant="outline-dark" onClick={handleDelete(reservation._id)}>
-                <FaRegTrashAlt className={styles.svgIcon} />
-              </Button>
-            </div>
+                <Button
+                  variant="outline-dark"
+                  onClick={() => navigate(`/ad/${reservation.adInfo}`)}
+                >
+                  <FaEye className={styles.svgIcon} />
+                </Button>
+                <p>Date: {reservation.reservationDate}</p>
+                <p>Time: {reservation.reservationTime}</p>
+                <Button
+                  variant="outline-dark"
+                  onClick={() => navigate(`/edit/${reservation._id}`)}
+                >
+                  Edit reservation
+                </Button>
+
+                <Button
+                  variant="outline-dark"
+                  onClick={() => handleDelete(reservation._id)}
+                >
+                  <FaRegTrashAlt className={styles.svgIcon} />
+                </Button>
+              </div>
             </div>
           ))}
         </div>
       )}
+      {/* Modal */}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Deletion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete this reservation?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={confirmDelete}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
